442e7f7d345647a5f37f7a007ebeb3f7bfa7754a
Abdou SEYE <abdou.seye@gsietechnology.com>
2023-06-13 00:47:37 +0000

    Structuration vues et templates login et inscription

---
+++ app/static/js/registration.js
@@ -0,0 +1,79 @@
+// user-registration.js
+
+// Get the registration form
+const registrationForm = document.getElementById('registration-form');
+
+// Add submit event listener to the form
+registrationForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Get the form inputs
+  const usernameInput = document.getElementById('username');
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+  const confirmPasswordInput = document.getElementById('confirm-password');
+
+  // Validate the form inputs
+  const username = usernameInput.value.trim();
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+  const confirmPassword = confirmPasswordInput.value.trim();
+
+  console.log(username, email, password, confirmPassword);
+
+  let isValid = true;
+
+  if (username === '') {
+    displayError(usernameInput, 'Username is required');
+    isValid = false;
+  } else {
+    removeError(usernameInput);
+  }
+
+  if (email === '') {
+    displayError(emailInput, 'Email is required');
+    isValid = false;
+  } else {
+    removeError(emailInput);
+  }
+
+  if (password === '') {
+    displayError(passwordInput, 'Password is required');
+    isValid = false;
+  } else {
+    removeError(passwordInput);
+  }
+
+  if (confirmPassword === '') {
+    displayError(confirmPasswordInput, 'Please confirm your password');
+    isValid = false;
+  } else if (confirmPassword !== password) {
+    displayError(confirmPasswordInput, 'Passwords do not match');
+    isValid = false;
+  } else {
+    removeError(confirmPasswordInput);
+  }
+
+  if (isValid) {
+    // Submit the form or perform further actions
+    registrationForm.submit();
+  }
+});
+
+// Function to display an error message
+function displayError(inputElement, message) {
+  const errorElement = document.createElement('div');
+  errorElement.classList.add('error');
+  errorElement.textContent = message;
+  inputElement.classList.add('error');
+  inputElement.parentNode.appendChild(errorElement);
+}
+
+// Function to remove an error message
+function removeError(inputElement) {
+  const errorElement = inputElement.parentNode.querySelector('.error');
+  if (errorElement) {
+    errorElement.remove();
+  }
+  inputElement.classList.remove('error');
+}
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from .models import Utilisateur
 
 users = Blueprint("users", __name__)
@@ -7,3 +7,11 @@ users = Blueprint("users", __name__)
 @users.route("/")
 def index():
     return "Bienvenue dans le projet"
+
+@users.route("/login")
+def login():
+    return render_template('login.html')
+
+@users.route("/register", methods=['GET', 'POST'])
+def register():
+    return render_template('registration.html')
