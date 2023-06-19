bcc758350e6689a17a7e0dfb5d93ee706eed0eb8
abduRahman94 <sabdouseye@gmail.com>
2023-06-19 17:36:08 +0000

    Ajout fonctionnalité admin

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_migrate import Migrate
-
+from flask_admin import Admin
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -19,6 +19,7 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = "users_bp.user-login"
     Migrate(app, db)
+    Admin(app)
 
     # register blueprint
     from app.views import users, contrib, admin
